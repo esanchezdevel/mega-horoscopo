ef7abd8572c9ba4916b68977e07ce7ddacc42316
Enrique Sanchez <esanchez.devel@gmail.com>
2023-08-16 20:34:34 +0200

    feat: first commit with basic index working

---
+++ src/main/java/com/mega/horoscopo/MegaHoroscopoApplication.java
@@ -0,0 +1,13 @@
+package com.mega.horoscopo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaHoroscopoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaHoroscopoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mega/horoscopo/MegaHoroscopoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mega.horoscopo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegaHoroscopoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
